b5a36381ae1e3eb50093f0b3075cb7a64d69bcef
Stephanie Cruz <exscruzme@gmail.com>
2022-08-12 16:25:25 -0400

    spawning a tower when toggling placement mode

--- levels/Placeholder.gd
+++ levels/Placeholder.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
 var GridOverlay = preload("res://levels/GridOverlay.tscn")
-
-var isGridOff = true
-
-func _on_Button_pressed():
-	var grid = $GridOverlay
-	isGridOff = !isGridOff
-	if(isGridOff):
-		grid.queue_free()
-	else:
-		grid = GridOverlay.instance()
-		add_child(grid)
+var TemplateTower = preload("res://Towers/TemplateTower.tscn")
+var is_grid_off = true
 	
+func _on_ToggleGridButton_pressed():
+	var grid_nodes = get_tree().get_nodes_in_group("grid")
+	is_grid_off = !is_grid_off
+	if(is_grid_off):
+		for i in grid_nodes.size():
+			grid_nodes[i].queue_free()
+	else:
+		var new_grid = GridOverlay.instance()
+		add_child(new_grid)
+
+
+func _on_PlaceTowerButton_pressed():
+	if(!is_grid_off):
+		var new_tower = TemplateTower.instance()
+		new_tower.position = get_global_mouse_position()
+		add_child(new_tower)
+	else:
+		print("can't place a tower with grid off")	
