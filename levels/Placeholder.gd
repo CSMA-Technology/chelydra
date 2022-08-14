8315a254a36b24ebb5da82d457b18a2595dedf44
Stephanie Cruz <exscruzme@gmail.com>
2022-08-14 19:56:47 -0400

    refactoring a little

--- levels/Placeholder.gd
+++ levels/Placeholder.gd
@@ -2,23 +2,25 @@ extends Node2D
 
 var GridOverlay = preload("res://levels/GridOverlay.tscn")
 var TemplateTower = preload("res://Towers/TemplateTower.tscn")
-var is_grid_off = true
-	
-func _on_ToggleGridButton_pressed():
+var is_grid_on = false
+onready var placement_mode_button = $UIPlaceholder/Control/PlacementModeButton
+
+func _on_ToggleGridButton_toggled(grid_on):
 	var grid_nodes = get_tree().get_nodes_in_group("grid")
-	is_grid_off = !is_grid_off
-	if(is_grid_off):
-		for i in grid_nodes.size():
-			grid_nodes[i].queue_free()
-	else:
+	if(grid_on):
+		is_grid_on = true
+		placement_mode_button.disabled = false
 		var new_grid = GridOverlay.instance()
 		add_child(new_grid)
+	else:
+		is_grid_on = true
+		placement_mode_button.disabled = true
+		for i in grid_nodes.size():
+			grid_nodes[i].queue_free()
 
 
-func _on_PlaceTowerButton_pressed():
-	if(!is_grid_off):
+func _on_PlacementModeButton_toggled(placement_mode_on):
+	if(placement_mode_on):
 		var new_tower = TemplateTower.instance()
 		new_tower.position = get_global_mouse_position()
 		add_child(new_tower)
-	else:
-		print("can't place a tower with grid off")	
