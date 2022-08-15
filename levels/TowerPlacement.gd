b1c4ccba8eaa089b03f7aca71fd5901ebc8673d0
Stephanie Cruz <exscruzme@gmail.com>
2022-08-15 10:55:14 -0400

    changed max x value lol

--- levels/TowerPlacement.gd
+++ levels/TowerPlacement.gd
@@ -5,6 +5,6 @@ var cell_size = 64
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		var snap_position = Vector2(clamp(int(event.position.x/cell_size), 0, 21), clamp(int(event.position.y/cell_size), 0, 11))
+		var snap_position = Vector2(clamp(int(event.position.x/cell_size), 0, 23), clamp(int(event.position.y/cell_size), 0, 11))
 		tower.position = snap_position*64
 		print("Mouse at : ", snap_position)
