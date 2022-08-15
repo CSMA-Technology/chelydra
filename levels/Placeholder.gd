358cd21c5cb30e0c6b9a8891c6cab6d02cea8193
Stephanie Cruz <exscruzme@gmail.com>
2022-08-15 16:30:48 -0400

    consolidating grid into tower placement scene, toggling visibility of placement scene instead of instancing it, placing towers only if mouse within placement area

--- levels/Placeholder.gd
+++ levels/Placeholder.gd
@@ -1,33 +1,17 @@
 extends Node2D
 
-var GridOverlay = preload("res://levels/GridOverlay.tscn")
 var TowerPlacement = preload("res://levels/TowerPlacement.tscn")
 var is_grid_on = false
-onready var placement_mode_button = $UIPlaceholder/Control/PlacementModeButton
+export var placement_mode = false
 
-func _on_ToggleGridButton_toggled(grid_on):
-	var grid_nodes = get_tree().get_nodes_in_group("grid")
-	if(grid_on):
-		is_grid_on = true
-		placement_mode_button.disabled = false
-		var new_grid = GridOverlay.instance()
-		add_child(new_grid)
+func _process(_delta):
+	if (placement_mode):
+		$TowerPlacement.show()
 	else:
-		is_grid_on = false
-		placement_mode_button.disabled = true
-		for i in grid_nodes.size():
-			grid_nodes[i].queue_free() # is there a better way to do this?
-
-func _on_PlacementModeButton_toggled(placement_mode_on):
-	var placement_nodes = get_tree().get_nodes_in_group("tower_placement")
-	if(placement_mode_on):
-		var tower_placement = TowerPlacement.instance()
-		add_child(tower_placement)
-		if tower_placement.has_signal("place_tower"):
-			tower_placement.connect("place_tower", self, "place_tower")
-	else:
-		for i in placement_nodes.size():
-			placement_nodes[i].queue_free()
+		$TowerPlacement.hide()
+		
+func _on_PlacementModeButton_toggled(placement_mode_toggle):
+	placement_mode = placement_mode_toggle
 
 func place_tower(position, tower):
 #	print("place a tower here: ", position)
