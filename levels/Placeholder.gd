661dd5d7893800b61db7bdc06d0bb625872c7e2a
Stephanie Cruz <exscruzme@gmail.com>
2022-08-15 11:23:24 -0400

    placing towers on the grid

--- levels/Placeholder.gd
+++ levels/Placeholder.gd
@@ -13,13 +13,25 @@ func _on_ToggleGridButton_toggled(grid_on):
 		var new_grid = GridOverlay.instance()
 		add_child(new_grid)
 	else:
-		is_grid_on = true
+		is_grid_on = false
 		placement_mode_button.disabled = true
 		for i in grid_nodes.size():
-			grid_nodes[i].queue_free()
+			grid_nodes[i].queue_free() # is there a better way to do this?
 
+func place_tower(position, tower):
+	print("place a tower here: ", position)
+	print(tower)
+	var new_tower = tower.instance()
+	new_tower.position = position
+	add_child(new_tower)
 
 func _on_PlacementModeButton_toggled(placement_mode_on):
+	var placement_nodes = get_tree().get_nodes_in_group("tower_placement")
 	if(placement_mode_on):
-		var new_tower = TowerPlacement.instance()
-		add_child(new_tower)
+		var tower_placement = TowerPlacement.instance()
+		add_child(tower_placement)
+		if tower_placement.has_signal("place_tower"):
+			tower_placement.connect("place_tower", self, "place_tower")
+	else:
+		for i in placement_nodes.size():
+			placement_nodes[i].queue_free()
--- levels/TowerPlacement.gd
+++ levels/TowerPlacement.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 onready var tower = $TemplateTower
+var tower_scene = preload("res://Towers/TemplateTower.tscn")
 var cell_size = 64
+
+signal place_tower(position, tower)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		var snap_position = Vector2(clamp(int(event.position.x/cell_size), 0, 23), clamp(int(event.position.y/cell_size), 0, 11))
-		tower.position = snap_position*64
-		print("Mouse at : ", snap_position)
+		var snap_position = calculate_snap_position(event.position)
+		tower.position = snap_position*cell_size
+#		print("Mouse at : ", snap_position)
+	if event is InputEventMouseButton and event.pressed:
+		emit_signal("place_tower", calculate_snap_position(event.position)*cell_size, tower_scene) # how will we pass the packed scene of different tower types?
+
+func calculate_snap_position(actual_position):
+	return Vector2(clamp(int(actual_position.x/cell_size), 0, 23), clamp(int(actual_position.y/cell_size), 0, 11))
