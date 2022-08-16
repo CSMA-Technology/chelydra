f6a1e040e525e4d2dd24e0d97ab735f4579cc3bd
Adrian Moya <moyaa1022@gmail.com>
2022-08-16 15:27:51 -0400

    feat(enemy-spawns): added enemy spawns

--- levels/TowerPlacement.gd
+++ levels/TowerPlacement.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-onready var tower = $TemplateTower
+onready var tower = $TowerPlaceholder
 var tower_scene = preload("res://Towers/TemplateTower.tscn")
 var cell_size = 64
 
+const GREEN = "0c9e20"
+const RED = "f1270c"
+
 signal place_tower(position, tower)
+
+func _process(delta):
+	if is_placement_valid():
+		$TowerPlaceholder/ColorRect.color = GREEN
+	else:
+		$TowerPlaceholder/ColorRect.color = RED
 
 func _input(event):
 	if (visible):
@@ -15,8 +24,11 @@ func _input(event):
 			var placement_area = $TileMap.get_used_rect()
 			print_debug("placement area: ", placement_area.position, " to ", placement_area.end)
 			print_debug("placement point: ", event.position/cell_size)
-			if (placement_area.has_point(event.position/cell_size)):
+			if (placement_area.has_point(event.position/cell_size) && is_placement_valid()):
 				emit_signal("place_tower", calculate_snap_position(event.position)*cell_size, tower_scene)
 
 func calculate_snap_position(actual_position):
 	return Vector2(clamp(int(actual_position.x/cell_size), 0, 23), clamp(int(actual_position.y/cell_size), 0, 11))
+
+func is_placement_valid():
+	return tower.get_overlapping_areas().size() == 0 && tower.get_overlapping_bodies().size() == 0
