d07ee8c1f0503d0fb9967ea5ca2a5baafad7a440
Stephanie Cruz <exscruzme@gmail.com>
2022-08-16 16:07:11 -0400

    fixed merge conflicts

--- levels/TemplateLevel.gd
+++ levels/TemplateLevel.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+onready var score_label = $ScoreLabel
 var TowerPlacement = preload("res://levels/TowerPlacement.tscn")
 var enemy = preload("res://Enemies/TemplateEnemy.tscn")
 var is_grid_on = false
 export var placement_mode = false
+export var lives = 3
+
+func _ready(): 
+	update_score_label(lives)
 
 func _process(_delta):
 	if (placement_mode):
@@ -20,9 +25,18 @@ func place_tower(position, tower):
 	$Towers.add_child(new_tower)
 	$Navigation.update_nav_area()
 
-
 func spawn_enemy(position):
 	var new_enemy = enemy.instance()
 	new_enemy.position = position
 	new_enemy.debug_mode = true
 	$Enemies.add_child(new_enemy)
+
+func update_score_label(score):
+	score_label.text = "LIVES LEFT: " + str(score)
+
+func _on_Enemy_reached_goal():
+	print_debug("enemy reached goal!")
+	lives -= 1
+	if (lives <= 0):
+		get_tree().quit()
+	update_score_label(lives)
