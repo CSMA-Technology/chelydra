b4562cd06f81afda99391df656c42db793b1ed65
admoya <moyaa1022@gmail.com>
2022-08-16 12:19:08 -0400

    feat(navigation): navigate around towers in real time (#8)

--- Enemies/TemplateEnemy.gd
+++ Enemies/TemplateEnemy.gd
@@ -34,6 +34,7 @@ func _physics_process(delta):
 		move_and_collide(movement)
 
 func update_path():
+	Navigation2DServer.map_force_update(get_world_2d().get_navigation_map())
 	path = Navigation2DServer.map_get_path(get_world_2d().get_navigation_map(), global_position, goal, false)
 	$Debug/PathLine2d.points = path
 	next_destination_idx = 0
--- levels/Navigation.gd
+++ levels/Navigation.gd
@@ -3,9 +3,14 @@ extends TileMap
 const TOWER_SIZE = 64
 const BUFFER = 1
 func _ready():
+	update_nav_area()
+
+func update_nav_area():
 	for tower in $"../Towers".get_children():
 		var starting_cell_v = tower.global_position/cell_size
 		for offset_x in range(-BUFFER, TOWER_SIZE/cell_size.x + BUFFER):
 			for offset_y in range(-BUFFER, TOWER_SIZE/cell_size.y + BUFFER):
 				var offset_v = Vector2(offset_x, offset_y)
 				set_cellv(starting_cell_v + offset_v, -1)
+	update_dirty_quadrants()
+	get_tree().call_group("Navigators", "update_path")
