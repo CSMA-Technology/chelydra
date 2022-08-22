b957b1559047db247c5c243cafb78aa49f6798ce
Stephanie Cruz <exscruzme@gmail.com>
2022-08-22 17:03:33 -0400

    moving things around

--- Scenes/Utils/GameManager.gd
+++ Scenes/Utils/GameManager.gd
@@ -1,8 +1,8 @@
 extends Node
 
+# TODO update these to get rid of template towers when necessary
 enum TowerEnum { TEMPLATE_TOWER, ERASER, TEMPLATE_PROJECTILE_TOWER }
-
-var towers = { # TODO update these to get rid of template towers when necessary
+var towers = { 
 	TowerEnum.TEMPLATE_TOWER: "res://Scenes/Towers/TemplateTower.tscn",
 	TowerEnum.ERASER: "res://Scenes/Towers/Eraser.tscn",
 	TowerEnum.TEMPLATE_PROJECTILE_TOWER: "res://Scenes/Towers/TemplateProjectileTower/TemplateProjectileTower.tscn"
