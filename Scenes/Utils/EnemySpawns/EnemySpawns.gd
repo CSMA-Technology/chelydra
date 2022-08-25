18c2749e672ce30abed21ddb2d16d1dba2a2d272
Adrian Moya <moyaa1022@gmail.com>
2022-08-25 18:40:48 -0400

    fix(enemies): multiplying speed by delta

--- Scenes/Utils/EnemySpawns/EnemySpawns.gd
+++ Scenes/Utils/EnemySpawns/EnemySpawns.gd
@@ -4,6 +4,12 @@ export var spawn_interval = 5
 export var plus_minus = 1
 export var is_spawning = false setget handle_is_spawning
 
+var spawn_positions = []
+
+func _ready():
+	for cell in get_used_cells():
+		spawn_positions.append(to_global(map_to_world(cell)))
+
 signal spawn_enemy(position, enemy)
 
 func handle_is_spawning(val):
@@ -17,15 +23,15 @@ func maybe_start_timer():
 		$SpawnTimer.start(spawn_interval + ((randf() * plus_minus) - plus_minus))
 
 func _on_SpawnTimer_timeout():
-	spawn()
+	if is_spawning:
+		spawn()
 	maybe_start_timer()
 
 func spawn():
-	if is_spawning:
-		var cells = get_used_cells()
-		var random_cell = cells[randi() % cells.size()]
-		var center_point = (random_cell * cell_size) + Vector2(cell_size.x/2, cell_size.y/2)
-		emit_signal("spawn_enemy", center_point, load(choose_enemy()))
+	var cells = get_used_cells()
+	var random_cell = cells[randi() % cells.size()]
+	var center_point = (random_cell * cell_size) + Vector2(cell_size.x/2, cell_size.y/2)
+	emit_signal("spawn_enemy", center_point, load(choose_enemy()))
 
 func choose_enemy():
 	var choice = GameManager.EnemyEnum.keys()[randi() % GameManager.EnemyEnum.size()]
